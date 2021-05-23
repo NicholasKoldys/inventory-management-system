af9a379b9e29ad3ec9007cd84d954a6809823b05
Nicholas Koldys <nicholas.koldys@gmail.com>
2021-05-23 14:02:16 -0500

    INIT
    project initialized with maven-archetype-simple

---
+++ src/main/java/dev/nicholaskoldys/wgu/inventory-managment-system/App.java
@@ -0,0 +1,13 @@
+package dev.nicholaskoldys.wgu.inventory-managment-system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
