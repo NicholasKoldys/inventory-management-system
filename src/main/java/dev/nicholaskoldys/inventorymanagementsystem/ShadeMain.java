c874fb8171d20e574c513284de437c5b9bce703c
Nicholas Koldys <nicholas.koldys@gmail.com>
2021-06-02 15:49:02 -0500

    Remove temp placeholder files, add maven build points, temp remove module-info.java

--- src/main/java/dev/nicholaskoldys/inventorymanagementsystem/ShadeMain.java
+++ src/main/java/dev/nicholaskoldys/inventorymanagementsystem/ShadeMain.java
@@ -3,6 +3,6 @@ package dev.nicholaskoldys.inventorymanagementsystem;
 public class ShadeMain {
 
     public static void main(String[] args) {
-        MainApp.main(args);
+        InventoryManagementSystem.main(args);
     }
 }
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module inventorymanagementsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens dev.nicholaskoldys.inventorymanagementsystem to javafx.fxml;
-    exports dev.nicholaskoldys.inventorymanagementsystem;
-}
